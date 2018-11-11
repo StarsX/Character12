48750d638c0f74e4e439847acb16059c5809552d
Tianchen Xu <TianchenX@outlook.com>
2018-11-11 09:52:16 +0800

    Update shaders.

--- Character12/Content/Shaders/PSBasePass.hlsl
+++ Character12/Content/Shaders/PSBasePass.hlsl
@@ -9,6 +9,7 @@
 float4 main(PS_Input input) : SV_TARGET
 {
 	const float3 norm = normalize(input.Norm);
+	const float lightAmt = max(dot(norm, float3(-0.5, 1.0, -1.0)), 0.0);
 
-	return float4(dot(norm, float3(-0.5, 1.0, -1.0)).xxx, 1.0f);
+	return float4((lightAmt + 0.25).xxx, 1.0);
 }

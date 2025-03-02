e7b076dc0186ef0cefb5d5aa2dce625b64a017df
StarsX <X-Universe@outlook.com>
2025-03-02 09:21:08 +0800

    Refine cmd-line arg parser and update DX SDK

--- Character12/CharacterX.cpp
+++ Character12/CharacterX.cpp
@@ -368,16 +368,35 @@ void CharacterX::OnMouseLeave()
 
 void CharacterX::ParseCommandLineArgs(wchar_t* argv[], int argc)
 {
+	const auto str_tolower = [](wstring s)
+	{
+		transform(s.begin(), s.end(), s.begin(), [](wchar_t c) { return towlower(c); });
+
+		return s;
+	};
+
+	const auto isArgMatched = [&argv, &str_tolower](int i, const wchar_t* paramName)
+	{
+		const auto& arg = argv[i];
+
+		return (arg[0] == L'-' || arg[0] == L'/')
+			&& str_tolower(&arg[1]) == str_tolower(paramName);
+	};
+
+	const auto hasNextArgValue = [&argv, &argc](int i)
+	{
+		const auto& arg = argv[i + 1];
+
+		return i + 1 < argc && arg[0] != L'/' &&
+			(arg[0] != L'-' || (arg[1] >= L'0' && arg[1] <= L'9') || arg[1] == L'.');
+	};
+
 	DXFramework::ParseCommandLineArgs(argv, argc);
 
 	for (auto i = 1; i < argc; ++i)
 	{
-		if (wcsncmp(argv[i], L"-warp", wcslen(argv[i])) == 0 ||
-			wcsncmp(argv[i], L"/warp", wcslen(argv[i])) == 0)
-			m_deviceType = DEVICE_WARP;
-		else if (wcsncmp(argv[i], L"-uma", wcslen(argv[i])) == 0 ||
-			wcsncmp(argv[i], L"/uma", wcslen(argv[i])) == 0)
-			m_deviceType = DEVICE_UMA;
+		if (isArgMatched(i, L"warp")) m_deviceType = DEVICE_WARP;
+		else if (isArgMatched(i, L"uma")) m_deviceType = DEVICE_UMA;
 	}
 }
 
